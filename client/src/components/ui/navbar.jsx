49455c5853245ac437c3b3a6788a93aaf99ae55f
Martin Flasar <christian.flasar@gmail.com>
2023-05-05 03:52:11 +0200

    Revert " Fix for Navbar not changing to Desktop/Mobile mode when resized"
    This reverts commit b6cbc1fd912024210f04ed582f7dbb3da5ca502c.

--- client/src/components/ui/navbar.jsx
+++ client/src/components/ui/navbar.jsx
@@ -25,13 +25,13 @@ function Navigation () {
     small: 800
   }
 
-  React.useEffect(() => {
+  window.addEventListener('resize', () => {
     if (window.innerWidth > screenSizes.small) {
       setDesktopMode(true)
     } else {
       setDesktopMode(false)
     }
-  }, [])
+  })
 
   const logout = async () => {
     await fetch('/api/auth/signout', {
