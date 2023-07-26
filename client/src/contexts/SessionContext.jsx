d89e9a196bbb11ce9f333672ed526d9f8b17a315
Martin Flasar <christian.flasar@gmail.com>
2023-07-26 22:51:29 +0200

    BDOGT-39 Frontend - Fixed wrong check for Forbidden statusText after request made. Backend - Removed unnecessary code.

--- client/src/contexts/SessionContext.jsx
+++ client/src/contexts/SessionContext.jsx
@@ -28,9 +28,10 @@ const SessionProvider = ({ children }) => {
   }
 
   const unauthorizedInterceptor = (response) => {
+    console.log(response)
     if (response.status === 401) {
       handleUnauthorized()
-    } else if (response.message === 'No token provided!') {
+    } else if (response.statusText === 'Forbidden') {
       handleUnauthorized()
     }
     return response
