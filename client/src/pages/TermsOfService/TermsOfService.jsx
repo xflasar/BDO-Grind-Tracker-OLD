a579d846f1b7d0cf33945bb00c182b15b98c5c08
Martin Flasar <christian.flasar@gmail.com>
2023-08-08 23:29:41 +0200

    Frontend - Added Terms Of Service.

--- client/src/pages/TermsOfService/TermsOfService.jsx
+++ client/src/pages/TermsOfService/TermsOfService.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-
+import '../../assets/pages/TermsOfService/TermsOfService.scss'
 const TermsOfService = () => {
   return (
     <div>
-      <section className='termsofservice'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12'>
-              <h2>Terms of Service</h2>
-              <p>
-                By accessing this web site, you are agreeing to be bound by
-                these web site Terms of Service, all applicable laws and
-                regulations, and agree that you are responsible for compliance
-                </p>
-            </div>
-          </div>
-        </div>
+      <section className='termsofservice-container'>
+        <h1>Terms of Service</h1>
+        <h6>Terms of Service updated on August 8, 2023</h6>
+
+        <h2>Accounts</h2>
+        <p>Usernames and any other data deemed unacceptable will be changed or result in a permanent restriction of your account.</p>
+        <p>Accounts can be deleted or deactivated by contacting us (all account-related data is also deleted).</p>
+
+        <h2>Piracy</h2>
+        <p>Any usage or extraction of code AND/OR data without express permission by owners is strictly prohibited and violates copyright law.</p>
+
+        <p>BDO-Grind-Tracker.com reserves the right to change any or all parts of this User Agreement at its sole discretion and without notice.</p>
       </section>
     </div>
   )
