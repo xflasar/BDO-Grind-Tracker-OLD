8c574c46d927e432bed8564309df15a0e887ce8b
Martin Flasar <christian.flasar@gmail.com>
2024-01-23 23:48:06 +0100

    Site Graph legend added and style updated.

--- client/src/components/ui/pages/Homepage/SessionsSummaryGraph.jsx
+++ client/src/components/ui/pages/Homepage/SessionsSummaryGraph.jsx
@@ -5,7 +5,7 @@ import PieChart from '../../../helpers/Graph/PieChart'
 const SessionSummaryGraph = () => {
   const sampleData = [
     {
-      name: 'Site 1',
+      name: 'Sycraia Abyssal Ruins',
       value: 10,
       color: 'green'
     },
@@ -23,6 +23,21 @@ const SessionSummaryGraph = () => {
       name: 'Site 4',
       value: 40,
       color: 'pink'
+    },
+    {
+      name: 'Site 5',
+      value: 15,
+      color: 'white'
+    },
+    {
+      name: 'Site 6',
+      value: 25,
+      color: 'orange'
+    },
+    {
+      name: 'Site 7',
+      value: 33,
+      color: 'purple'
     }
   ]
 
@@ -31,9 +46,26 @@ const SessionSummaryGraph = () => {
       <h1>Site Graph</h1>
       <PieChart data={sampleData} />
       <div className='siteGraph-legend'>
+        <div className='siteGraph-legend-text'>
         {sampleData.map((item, index) => {
           return <span key={index} style={{ color: item.color }}>{item.name}</span>
         })}
+        </div>
+        <div className='siteGraph-legend-textgraph'>
+          {sampleData.map((item, index) => {
+            // inefficient
+            const highestPercentage = Math.max(...sampleData.map(x => x.value))
+            console.log(highestPercentage)
+
+            const width = (item.value / highestPercentage) * 100
+
+            return (
+              <div key={index} className='item' style={{ color: item.color }}>
+                <div className='item-graphical' style={{ backgroundColor: item.color, width: `${width}%` }}/>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
